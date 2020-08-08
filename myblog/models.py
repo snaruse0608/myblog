32f7f350c6acc9b48bf0fcdd04f12104c4df011a
“snaruse0608” <kakimoto3868@gmail.com>
2020-08-08 18:43:49 +0900

    ユーザと記事モデルの紐付け

--- myblog/models.py
+++ myblog/models.py
@@ -1,10 +1,15 @@
+from accounts.models import CustomUser
 from django.db import models
 
 class Article(models.Model):
+    class Meta:
+        db_table = 'article'
+
     title = models.CharField(verbose_name='記事タイトル', max_length=40)
     content = models.TextField(verbose_name='本文')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_ad = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+    user = models.ForeignKey(CustomUser, verbose_name="ユーザー", on_delete=models.PROTECT)
     
     def __str__(self):
         return self.title
