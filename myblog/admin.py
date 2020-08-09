665a483ad78fc3c0a957de74214f7e18ec05352d
“snaruse0608” <kakimoto3868@gmail.com>
2020-08-09 17:48:12 +0900

    タグを追加

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Article
+from .models import Tag
 from markdownx.admin import MarkdownxModelAdmin
 
-admin.site.register(Article, MarkdownxModelAdmin)
+class AirticleAdmin(MarkdownxModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(Article, AirticleAdmin)
+admin.site.register(Tag)
--- myblog/models.py
+++ myblog/models.py
@@ -4,16 +4,26 @@ from django.db import models
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 
+class Tag(models.Model):
+    class Meta:
+        db_table = 'tag'
+
+    name = models.CharField(verbose_name='タグ名', max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     class Meta:
         db_table = 'article'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(verbose_name='記事タイトル', max_length=40)
-    content = MarkdownxField('Content', help_text='To Write with Markdown format')
+    tags = models.ManyToManyField(Tag, blank=True)
+    user = models.ForeignKey(CustomUser, verbose_name="ユーザー", on_delete=models.PROTECT)
+    content = MarkdownxField('本文', help_text='マークダウン形式で記述してください')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_ad = models.DateTimeField(verbose_name='更新日時', auto_now=True)
-    user = models.ForeignKey(CustomUser, verbose_name="ユーザー", on_delete=models.PROTECT)
 
     
     def __str__(self):
