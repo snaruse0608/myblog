10fcd13f109a87c134aa9d720497278dfe7c23e1
“snaruse0608” <kakimoto3868@gmail.com>
2020-08-14 00:38:49 +0900

    test追加

--- config/settings_common.py
+++ config/settings_common.py
@@ -6,8 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
-ALLOWED_HOSTS = env('ALOWED_HOSTS')
-
 SECRET_KEY = env('SECRET_KEY')
 
 INSTALLED_APPS = [
