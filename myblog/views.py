3b7d0babdf4fdf065a4ec6ac2dbc0e4154ade8bc
osechitan <toshine212@hotmail.co.jp>
2020-10-11 14:43:51 +0900

    閲覧数機能追加

--- myblog/views.py
+++ myblog/views.py
@@ -12,3 +12,11 @@ class ArticleListView(generic.ListView):
 class ArticleDetailView(generic.DetailView):
     model = Article
     template_name = 'detail.html'
+
+    # 取得した記事の閲覧数を+1する
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article = context.get("object")
+        article.views += 1
+        article.save()
+        return context
