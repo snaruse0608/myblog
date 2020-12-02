f841183a6382f59ed7dd403a7a7554dacf04b52a
“snaruse0608” <kakimoto3868@gmail.com>
2020-12-02 23:34:42 +0900

    タグ一覧ページを作成

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'myblog'
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='Index'),
-    path('detail/<pk>/', views.ArticleDetailView.as_view(), name='Detail')
+    path('detail/<pk>/', views.ArticleDetailView.as_view(), name='Detail'),
+    path('tag', views.TagListView.as_view(), name='TagIndex'),
+    path('article/tag/<pk>/', views.ArticleByTagListView.as_view(), name='ArticleByTagIndex')
 ]
