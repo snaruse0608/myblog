ecf187be594f6122f89c14f47aa96b15656d4503
osechitan <osechitan@gmail.com>
2020-12-18 23:23:22 +0900

    fix html files

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,8 +5,19 @@ from .models import CustomUser
 
 class CusomtUserAdmin(UserAdmin):
     fieldsets = (
-        (('User info'), {'fields': ('username', 'email', 'first_name', 'last_name', 'introduction')}),)
-    list_display = ('username', 'email', 'first_name', 'last_name', 'introduction')
+        ('User info', {
+                'fields': ('username',
+                           'email',
+                           'first_name',
+                           'last_name',
+                           'introduction')
+        }),
+    )
+    list_display = ('username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'introduction')
 
 
 admin.site.register(CustomUser, CusomtUserAdmin)
